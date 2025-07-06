aa795d6e6a08f4a4f95f99956fba14ed9db42fff
Neroimor <neroimor@gmail.com>
2025-07-06 03:10:40 +0300

    added Linear Search

---
+++ Search/ISearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Search
+{
+    internal interface ISearch<T>  where T : IComparable<T>
+    {
+
+        public int Search(T[] array, T value);
+    }
+}
